0a3dbe273f5350afe0cc381a4437974ce588c9a1
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-09-11 01:55:55 -0500

    update server to initialize twilio client

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const twilio = require('twilio')
+require('dotenv').config()
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID
+const authToken = process.env.TWILIO_AUTH_TOKEN
+
+
+const client = require('twilio')(accountSid, authToken)
 
 
 const accoun
